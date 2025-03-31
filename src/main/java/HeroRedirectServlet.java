528b60ccf7c3ea16a4e844e2b7db620f6f36cf72
kirillKozitskiy <kkozickij35@gmail.com>
2025-03-31 19:59:00 +0300

    Initial commit

---
+++ src/main/java/HeroRedirectServlet.java
@@ -0,0 +1,16 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/hero")
+public class HeroRedirectServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("second-task/hero.html");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Choose the page</title>
+    </head>
+    <body>
+        <h2>
+            <a href="first-task/verse.html">First task</a>
+        </h2>
+        <br><br>
+        <h2>
+            <a href="second-task/hero.html">Second Task</a>
+        </h2>
+    </body>
+</html>
